782f7f22bd646989bd28f6a83c45e8cf248dddcb
esksko <esksko@gmail.com>
2025-11-14 15:49:20 +0100

    Added data and Map page

---
+++ apps/pages/7_Map.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Map and selectors", layout="wide", initial_sidebar_state="expanded")
+
+st.title("Map and selectors")
+st.sidebar.title("Navigation")
